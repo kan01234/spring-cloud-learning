91c4c2f7dde693e991d846713abd33e304729e5f
Ken Leung <ken.leung@ifshk.com>
2018-04-26 10:27:41 +0100

    init commit

---
+++ SpringCloudConfigClient/src/main/java/com/dotterbear/spring/cloud/config/client/SpringCloudConfigClientApplication.java
@@ -0,0 +1,24 @@
+package com.dotterbear.spring.cloud.config.client;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringCloudConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConfigClientApplication.class, args);
+	}
+
+	@Value("${bar}")
+	String bar;
+
+	@RequestMapping(value = "/bar")
+	public String hi() {
+		return bar;
+	}
+}
