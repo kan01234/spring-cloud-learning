61b78e81113b67a4ee15101cdc217f4ab905b1be
Ken Leung <ken.leung@ifshk.com>
2018-04-26 09:50:19 +0100

    update port number

--- SpringCloudServiceRibbon/src/main/java/com/dotterbear/spring/cloud/service/ribbon/SpringCloudServiceRibbonApplication.java
+++ SpringCloudServiceRibbon/src/main/java/com/dotterbear/spring/cloud/service/ribbon/SpringCloudServiceRibbonApplication.java
@@ -2,6 +2,7 @@ package com.dotterbear.spring.cloud.service.ribbon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
@@ -9,6 +10,8 @@ import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -25,6 +28,16 @@ public class SpringCloudServiceRibbonApplication {
 	@LoadBalanced
 	RestTemplate restTemplate() {
 		return new RestTemplate();
+	}
+
+	@Bean(name = "hystrixRegistrationBean")
+	public ServletRegistrationBean<HystrixMetricsStreamServlet> servletRegistrationBean() {
+		ServletRegistrationBean<HystrixMetricsStreamServlet> registration = new ServletRegistrationBean<HystrixMetricsStreamServlet>(
+				new HystrixMetricsStreamServlet(),
+				"/hystrix.stream");
+		registration.setName("hystrixServlet");
+		registration.setLoadOnStartup(1);
+		return registration;
 	}
 
 }
