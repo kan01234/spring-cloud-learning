ffc92e1f906cd3b0d9976b37fc4e412d6d1ced1b
kan <safghjkl@gmail.com>
2018-04-27 10:05:27 +0800

    add registry to config server, client

--- SpringCloudConfigClient/src/main/java/com/dotterbear/spring/cloud/config/client/SpringCloudConfigClientApplication.java
+++ SpringCloudConfigClient/src/main/java/com/dotterbear/spring/cloud/config/client/SpringCloudConfigClientApplication.java
@@ -3,11 +3,13 @@ package com.dotterbear.spring.cloud.config.client;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@EnableDiscoveryClient
 public class SpringCloudConfigClientApplication {
 
 	public static void main(String[] args) {
@@ -18,7 +20,7 @@ public class SpringCloudConfigClientApplication {
 	String bar;
 
 	@RequestMapping(value = "/bar")
-	public String hi() {
+	public String bar() {
 		return bar;
 	}
 }
