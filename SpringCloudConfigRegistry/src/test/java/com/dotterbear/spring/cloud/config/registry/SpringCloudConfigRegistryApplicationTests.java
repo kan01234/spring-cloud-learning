625617e4dd7ed94eda00b23161f3b3467b037b9c
kan <safghjkl@gmail.com>
2018-04-27 10:04:34 +0800

    add spring cloud config registry

---
+++ SpringCloudConfigRegistry/src/test/java/com/dotterbear/spring/cloud/config/registry/SpringCloudConfigRegistryApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dotterbear.spring.cloud.config.registry;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringCloudConfigRegistryApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
